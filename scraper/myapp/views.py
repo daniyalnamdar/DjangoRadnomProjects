849435ba1252f9753d911ad1fb2688e84c7ad4bd
Daniyal Namdar <daniyal.namdar@bktomorrow.com>
2025-07-23 20:17:47 +0300

    implement the scrape from static web

--- scraper/myapp/views.py
+++ scraper/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
+
+def scrape(request):
+    page = requests.get('https://www.facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    linkAddress = []
+    
+    for link in soup.find_all('a'):
+        linkAddress.append(link.get('href'))
+        
+    return render(request, 'myapp/result.html',{'linkAddress': linkAddress})
